364f282515cf71293c6eee46de664687edb6cf54
Greg Annandale <greg@gregannandale.com>
2026-03-16 23:02:38 +0000

    hdi v0.5.0 release

--- hdi.rb
+++ hdi.rb
@@ -1,8 +1,8 @@
 class Hdi < Formula
   desc "Extract install & run commands from a project's README"
   homepage "https://github.com/grega/hdi"
-  url "https://github.com/grega/hdi/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "fb1e7676c84310d51a8193315ed28ca2ffe95d3ea4a778d0dbdabac50aaf5d4d"
+  url "https://github.com/grega/hdi/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   def install
