b638b3332a3ff1acd60ab3d1a294db3b7aadcef2
Greg Annandale <greg@gregannandale.com>
2026-03-17 09:16:31 +0000

    Update description to include 'test' commands

--- Formula/hdi.rb
+++ Formula/hdi.rb
@@ -1,5 +1,5 @@
 class Hdi < Formula
-  desc "Extract install & run commands from a project's README"
+  desc "Extract install, run & test commands from a project's README"
   homepage "https://github.com/grega/hdi"
   url "https://github.com/grega/hdi/archive/refs/tags/v0.7.0.tar.gz"
   sha256 "6101a36cd368cd4847897c5b97db5e7aff0fbcd08d8fab3d0ec19c515114689d"
