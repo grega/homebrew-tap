87370f85448323b72ebdcdd5c6d285fd060f4e87
Greg Annandale <greg@gregannandale.com>
2026-04-01 21:40:58 +0100

    hdi v0.24.0 release

--- Formula/hdi.rb
+++ Formula/hdi.rb
@@ -1,8 +1,8 @@
 class Hdi < Formula
   desc "Extract install, run & test commands from a project's README"
   homepage "https://github.com/grega/hdi"
-  url "https://github.com/grega/hdi/releases/download/v0.23.1/hdi-v0.23.1.tar.gz"
-  sha256 "6842318c2976359d57352df94ecf9eff79e608eaebaf46ff7e173950621a76e8"
+  url "https://github.com/grega/hdi/releases/download/v0.24.0/hdi-v0.24.0.tar.gz"
+  sha256 "8e0e1020b60a79f50f9b4c0c6a9f03db212b35e28eb4a92f0e181014644ceb30"
   license "MIT"
 
   def install
